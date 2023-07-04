710c7b95d84859a49e145f496f5451d7040ee935
Jesus Hernandez <jahp88x@gmail.com>
2023-07-04 12:28:23 +0200

    new changes

--- README.md
+++
@@ -1,7 +0,0 @@
-# Repositorio
-
-Repositorio de portafolio CV
-
-## proyectos
-
-- [Landing Page de tipo portafolio-cv](https://JesusHernandezP.github.io/portafolio/portafolio-CV)
--- portafolio-CV/js/main.js
+++ portafolio-CV/js/main.js
@@ -55,3 +55,17 @@
     });
   })(document);
 
+  window.addEventListener("scroll", function() {
+    const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      scrollToTopBtn.style.display = "block";
+    } else {
+      scrollToTopBtn.style.display = "none";
+    }
+  });
+  
+  document.getElementById("scrollToTopBtn").addEventListener("click", function() {
+    document.body.scrollTop = 0; // Para navegadores webkit
+    document.documentElement.scrollTop = 0; // Para otros navegadores
+  });
+  
